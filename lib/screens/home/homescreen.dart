ff12462f6ef53f6133c6d5a6d0b010ce5eaf0c5f
BEARKUNN <tensorflownetwork@gmail.com>
2020-01-17 21:51:30 +0800

    Flutter app code

---
+++ lib/screens/home/homescreen.dart
@@ -0,0 +1,79 @@
+import 'package:financial_App/screens/tensorchatbotscreen/ChatBot.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ok'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        elevation: 16.0,
+        child: Column(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("Bobby"),
+              accountEmail: Text("bobby@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text("Bob"),
+              ),
+              otherAccountsPictures: <Widget>[
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Text("Jack"),
+                )
+              ],
+            ),
+            ListTile(
+              title: new Text("All Inboxes"),
+              leading: new Icon(Icons.mail),
+            ),
+            Divider(
+              height: 0.1,
+            ),
+            ListTile(
+              title: new Text("Bank Loan"),
+              leading: new Icon(Icons.attach_money),
+            ),
+            ListTile(
+              title: new Text("Social"),
+              leading: new Icon(Icons.people),
+            ),
+            ListTile(
+              title: new Text("Promotions"),
+              leading: new Icon(Icons.local_offer),
+            ),
+          ],
+        ),
+      ),
+      // body is the majority of the screen.
+      body: Center(
+        child: Text('Welcome to Tensor Financial App!',
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+                fontStyle: FontStyle.italic)),
+      ),
+      backgroundColor: Colors.grey[900],
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Chat with Tensor', // used by assistive technologies
+        child: Icon(Icons.message),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => HomePageDialogflow()),
+          );
+        },
+      ),
+    );
+  }
+}
